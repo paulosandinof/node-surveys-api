6d5f97297e176c370b8c7c7505d5be5fc5a336b3
Sandino <37051758+paulosandinof@users.noreply.github.com>
2022-04-07 18:47:35 -0300

    chore: remove protocols in coverage reports

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'],
   coverageDirectory: 'coverage'
 }
